0247700c83fd4038acfffd582a902df59dbbaf7e
bannafi <54008404+bannafi@users.noreply.github.com>
2020-07-20 23:11:15 +0700

    Create page & todo components

--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,38 +1,45 @@
 import React from "react";
+import Todo from "./todo";
 
 const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React!"
+    },
+    {
+      text: "Belajar props di React!"
+    },
+    {
+      text: "Belajar hooks React!"
+    },
+    {
+      text: "Belajar state React!"
+    },
+    {
+      text: "Belajar React!"
+    },
+    {
+      text: "Belajar React!"
+    },
+    {
+      text: "Belajar props di React!"
+    },
+    {
+      text: "Belajar hooks React!"
+    },
+    {
+      text: "Belajar state React!"
+    },
+    {
+      text: "Belajar React!"
+    }
+  ];
+
   return (
     <section class="todos">
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
-      <div class="todo">
-        <span class="todo-text">Learning React</span>
-      </div>
+      {todos.map(todo => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
