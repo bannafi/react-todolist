23f0c16b2bda7cb9431c395213fb7878335515f9
bannafi <54008404+bannafi@users.noreply.github.com>
2020-07-25 05:02:33 +0700

    added  addTodo props on TodoForm

--- src/components/todoForm.jsx
+++ src/components/todoForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState("");
 
   const handleFormSubmit = e => {
