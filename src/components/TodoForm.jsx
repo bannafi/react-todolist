b660c9b0a5ee741932ee471cc54b46660258e4da
bannafi <54008404+bannafi@users.noreply.github.com>
2020-08-16 06:18:23 +0700

    Create button Component

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const TodoForm = ({ addTodo, showAdd }) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("No blank input");
+      return;
+    }
+
+    if (value.length > 40) {
+      alert("Please enter a shorter todo text!");
+      setValue("");
+      return;
+    }
+
+    addTodo(value);
+    setValue("");
+  };
+
+  if (showAdd) {
+    return (
+      <section className="add">
+        <form className="add-form" onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className="add-input"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+          <button className="add-btn main-black-color">Add</button>
+        </form>
+      </section>
+    );
+  } else {
+    return null;
+  }
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired
+};
+
+export default TodoForm;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Todo from "./Todo";
+import PropTypes from "prop-types";
+
+const Todos = ({ todos, completeTodo }) => {
+  return (
+    <section className="todos">
+      {todos.length > 0 &&
+        todos.map((todo, index) => {
+          return (
+            <Todo
+              key={index}
+              text={todo.text}
+              isCompleted={todo.isCompleted}
+              completeTodo={completeTodo}
+              index={index}
+            />
+          );
+        })}
+      {todos.length === 0 && (
+        <div className="todo-placeholder-text">
+          Add todo by clicking the{" "}
+          <span className="add-button-placeholder-text">Add</span> button on the
+          top left corner
+        </div>
+      )}
+    </section>
+  );
+};
+
+Todos.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string
+    })
+  ),
+  completeTodo: PropTypes.func.isRequired
+};
+export default Todos;
